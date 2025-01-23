9375d678681e42d80ca63e3ac5cd337e0d25f30c
Barbara Barta <139867625+babura47@users.noreply.github.com>
2025-01-23 13:21:40 +0100

    Add files via upload

--- sparse_matrices.py
+++ sparse_matrices.py
@@ -6,16 +6,11 @@ Created on Thu Jan  9 14:51:52 2025
 """
 import time
 import itertools
-import cProfile
-import pstats
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import coo_array, eye, linalg, save_npz, load_npz, triu, tril, coo_matrix
-import matspy
-from numba import njit
+from scipy.sparse import coo_array, eye, linalg, coo_matrix
 import pickle
 import os
-import matplotlib
 os.chdir("C:\\Users\\barba\\Desktop\\thesis\\python")
 
 """
@@ -146,7 +141,8 @@ def plot_coo_matrix(m):
     return ax
 
 
-def solve_system2(Ns, params, policy):
+def solve_system2(Ns, params, policy,tol):
+    pt=time.time()
     N1,N2,N3,N4,N5 = Ns
     states2 = list(itertools.product(np.arange(N1), np.arange(N2+1), np.arange(N3), np.arange(N4), np.arange(N5), np.arange(N2)))
     nr_states2 =  len(states2)
@@ -173,7 +169,7 @@ def solve_system2(Ns, params, policy):
             servers_not_full = [s1_not_full, s2_not_full, s3_not_full, s4_not_full, s5_not_full]
             
             active_transitions = [
-                s1_not_full or s2_not_full, #and state[-1]<= 5,  # Arrival
+                s1_not_full or s2_not_full,  # Arrival
                 state[0] > 0 and (s3_not_full or s4_not_full),  # service at Q1
                 state[1] > 0 and (s4_not_full or s5_not_full),  # service at Q2
                 state[2] > 0,  # service at Q3
@@ -223,21 +219,31 @@ def solve_system2(Ns, params, policy):
     M = M[valid_indices, :]
     M = M[:, valid_indices]
     v = v[valid_indices]
-    wait_times2 = linalg.lsqr(M,v, atol = 1e-10, btol=1e-10)[0]
+    
+    tt = time.time()
+    print(f"making the matrix took {tt-pt:.2f} seconds")
+
+    
+    wait_times2 = linalg.lsqr(M,v, atol =tol, btol=tol)[0]
+    
+    wait_times2 = wait_times2 - 1/params[2] #take out expected S2 service time for tagged cust
+
     
     if np.any(wait_times2<-0.1):
         print("negative solutions")
     
-    #wait_times2[wait_times2<0] = 0
+    wait_times2[wait_times2<0] =0
+    print(f"solving took {time.time()-tt:.2f} seconds")
+    
     states2 = [s for s in states2 if s[1]-s[-1]>0]
     wait_times_dict = dict(zip(states2,wait_times2))
     wait_times_dict = {key: value for key, value in wait_times_dict.items() if key[-1] == 0}
 
     return wait_times_dict, M
 
-def solve_system1(Ns, params, policy):
+def solve_system1(Ns, params, policy, tol):
     [N1,N2,N3,N4,N5] = Ns
-
+    pt = time.time()
     states1 = list(itertools.product(np.arange(N1+1), np.arange(N2), np.arange(N3), np.arange(N4), np.arange(N5), np.arange(N1)))
     nr_states1 =  len(states1)
     it = 0
@@ -252,7 +258,6 @@ def solve_system1(Ns, params, policy):
         if state[0] - state[-1]<=0:
             b.append(0)
         else:
-            
             valid_indices.append(it)
             s1_not_full = state[0] <= N1-1 
             s2_not_full = state[1] <= N2-2 
@@ -261,11 +266,9 @@ def solve_system1(Ns, params, policy):
             s5_not_full = state[4] <= N5-2
             
             servers_not_full = [s1_not_full, s2_not_full, s3_not_full, s4_not_full, s5_not_full]
-            
-            
-            
+        
             active_transitions = [
-                s1_not_full or s2_not_full, #and state[-1]<= 5,  # Arrival
+                s1_not_full or s2_not_full,  # Arrival
                 state[0] > 0 and (s3_not_full or s4_not_full),  # service at Q1
                 state[1] > 0 and (s4_not_full or s5_not_full),  # service at Q2
                 state[2] > 0,  # service at Q3
@@ -282,6 +285,7 @@ def solve_system1(Ns, params, policy):
                     continue
                 
                 new_state, min_queue = transition1(list(state), j, params, servers_not_full, policy)
+        
                 if new_state[0] == new_state[-1]: #tagged customer finished service at S1
                     if min_queue == 2:
                         b[it] += probs[j]*(new_state[2]-1)/params[3]
@@ -314,17 +318,24 @@ def solve_system1(Ns, params, policy):
     M = M[valid_indices, :]
     M = M[:, valid_indices]
     v = v[valid_indices]
-    wait_times1 = linalg.lsqr(M,v, atol = 1e-10, btol=1e-10)[0]
-    
+    tt = time.time()
 
+    print(f"making the matrix took {tt-pt:.2f} seconds")
+
+    wait_times1 = linalg.lsqr(M,v, atol = tol, btol=tol)[0]
+    
+    wait_times1 = wait_times1 - 1/params[1] #takke out expected S1 service time for tagged cust
+    
     if np.any(wait_times1<-0.1):
         print("negative solutions")
+    
+    wait_times1[wait_times1<0] = 0
 
-    #wait_times1[wait_times1<0] = 0
+    print(f"solving took {time.time()-tt:.2f} seconds")
 
     
-
     states1 = [s for s in states1 if s[0]-s[-1]>0]
+
     wait_times_dict = dict(zip(states1,wait_times1))
     wait_times_dict = {key: value for key, value in wait_times_dict.items() if key[-1] == 0}
 
@@ -335,33 +346,34 @@ def solve_system1(Ns, params, policy):
 #%%
 
 start = time.time()
-N=7
-Ns = [N,N,N,N,N]
-params = np.array([1., 3., 3., 1., 1., 1.])
 
-policy= {}
+params = np.array([0.1, 1., 1., 1., 1., 1.])
+N1 = 5
+N2 = 5
+Ns = [N1,N1,N2,N2,N2]
+
 
 [N1,N2,N3,N4,N5] = Ns
 states = list(itertools.product(np.arange(N1), np.arange(N2), np.arange(N3), np.arange(N4), np.arange(N5)))
 
-for s in states:
-    s1 = list(s).copy(); s1[0]+=1
-    s2 = list(s).copy(); s2[1]+=1
-    policy[s] = 0 if s1[0]/params[1] <= s2[1]/params[2] else 1
+tol = 1e-15
 
-for i in range(10):
+policy= {}
+for s in states:
+    policy[s] = 0 if (s[0]+1)/params[1] <= (s[1]+1)/params[2] else 1
+
+for i in range(1,11):
     print(f"i = {i}")
     time1 = time.time()
-    wait_times1, M1 = solve_system1(Ns,params,policy)
+   
+    wait_times1, _ = solve_system1(Ns,params,policy,tol)
+
     time2 = time.time()
     print(f"time = {time2-time1:.2f} seconds")
-    wait_times2, M2 = solve_system2(Ns,params,policy)
+    wait_times2, _ = solve_system2(Ns,params,policy,tol)
     time3 = time.time()
     print(f"time = {time3-time2:.2f} seconds")
 
-    wait_times1dict = wait_times1
-    wait_times2dict = wait_times2
-    
     new_policy= {}
     
     for s in states:
@@ -384,6 +396,54 @@ end = time.time()
 
 print(f"in total this took {(end-start)/60:.2f} minutes")
 
+#%%
+#saving the policy
+name = str(params) + str(N1) +"_"+  str(N3) + ".pkl"
+with open(name, 'wb') as f:
+    pickle.dump(policy, f)
+    
+#%%
+#loading a saved policy
+name = str(params) + str(N1) +"_"+  str(N3) + ".pkl"
+with open(name, 'rb') as f:
+    policy = pickle.load(f)
+#%%
+
+
+states0 = [s for s in policy.keys() if s[0]==1 and s[-1]==0]  #here i have set n1=0 and n5=0
+policy0 = {s[1:4]:policy[s] for s in states0}
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+
+
+# Extract coordinates and values
+x, y, z, values = [], [], [], []
+for (xi, yi, zi), value in policy0.items():
+    x.append(xi)
+    y.append(yi)
+    z.append(zi)
+    values.append(value)
+
+# Create a 3D scatter plot
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111, projection='3d')
+
+# Map the values to colors
+colors = ['red' if v == 1 else 'blue' for v in values]
+
+# Plot the scatter
+scatter = ax.scatter(x, y, z, c=colors, marker='o', s=40)
+
+# Set labels
+ax.set_xlabel('$n_2$')
+ax.set_ylabel('$n_3$')
+ax.set_zlabel('$n_4$')
+ax.set_title('3D Scatter Plot')
+
+plt.show()
+
+#wait_times_dict = dict(zip(states1,wait_times1))
 
 #%%
 # real_states = [s for s in states if s[0]-s[-1]>=1]
@@ -415,27 +475,29 @@ Y,X = np.meshgrid(n1r, n2r)
 # plt.scatter(X[best_resp==1.5], Y[best_resp==1.5], color='yellow', label='Both routes optimal',marker=matplotlib.markers.MarkerStyle('o', fillstyle = 'top'))
 #%%
 
-N=10
+N=18
 n1r = np.array(range(N))
 n2r = np.array(range(N))
 
-pol = [[k[:2],v] for k,v in policy.items() if k[2:]==(0,0,0)]
+pol = [[k[:2],v] for k,v in policy.items() if k[2:]==(10,10,0)]
 
 x_coords = [item[0][0] for item in pol]
 y_coords = [item[0][1] for item in pol]
 colors = ['red' if item[1] == 1 else 'blue' if item[1]==0 else "green" for item in pol]
 
-plt.figure(figsize=(6, 6))
+plt.figure(figsize=(6,6))
 plt.scatter(x_coords, y_coords, c=colors, s=100)
-plt.title("Nash strategy $n_3=1,n_4=n_5=5$, all rates equal to 1")
-plt.xlabel("$n_1$")
-plt.ylabel("$n_2$")
+plt.title("Nash strategy $n_3=n_4=10, n_5=0$, $\lambda=2, \mu_1=\mu_2=1.5, \mu_3=\mu_4=\mu_5=1$", fontsize= 25)
+plt.xlabel("$n_1$", fontsize= 25)
+plt.ylabel("$n_2$", fontsize= 25)
 
 plt.scatter([], [], c='red', label='$S_2$')
 plt.scatter([], [], c='blue', label='$S_1$')
-plt.legend(title="Value", loc="upper right")
+plt.legend(title="Value", loc="upper right", fontsize= 25)
 
 plt.grid(True)
-plt.xticks(range(8))
-plt.yticks(range(8))
+plt.xticks(range(N))
+plt.yticks(range(N))
 plt.show()
+
+#%%
